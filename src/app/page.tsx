926e1f7f60bf067fccaca96b379dedfcae0518a8
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-01-02 15:41:24 +0530

    cardamom pricing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,15 +33,13 @@ export default function Home() {
   //   checkAuthentication();
   // }, [isAuthenticated, router]);
   return (
-    <div className="bg-[#000000] h-screen p-5">
-      <Image
-        src={"/cardamom (1).png"}
-        width={2000}
-        height={2000}
-        alt=""
-        className=""
-      />
-      {"w-[100vw] h-[100vh]"}
+    <div className="bg-[#000000] h-screen p-5 overflow-hidden">
+      <div
+        className="
+            w-[100vw] h-[100vh] absolute opacity-15 flex items-center justify-center"
+      >
+        <Image src={"/cardamom (1).png"} width={2000} height={2000} alt="" />
+      </div>
       <div className="flex  items-center">
         <Image
           src={"/cardamom (3).png"}
